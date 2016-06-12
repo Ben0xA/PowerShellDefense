4e9b81a69d4080bedae2e81b074aa5cf9bbf4b9b
Ben0xA <web@ben0xa.com>
2016-06-12 12:58:50 -0400

    Update Invoke-Ignorer.ps1
    Added impersonation and cleaned up the code.

--- Invoke-Ignorer.ps1
+++ Invoke-Ignorer.ps1
@@ -1,19 +1,38 @@
 function Invoke-Ignorer {
+	[CmdletBinding()]
 	Param()
 
+	# Add Member Defition for LogonUser
+	$api = Add-Type -Name Ignore -MemberDefinition @"
+		[DllImport("advapi32.dll", SetLastError = true)] 
+		public static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+"@ -passthru
+
+	# Get Credentals. Use \DOMAIN\User for username
 	$creds = Get-Credential
+	$user = $($creds.GetNetworkCredential().UserName)
+	$password = $($creds.GetNetworkCredential().Password)
+	$domain = $($creds.GetNetworkCredential().Domain)
+	$plain = "$($user):$($password)"
 
+	# Impersonate the new user
+	[IntPtr]$token = [Security.Principal.WindowsIdentity]::GetCurrent().Token
+	$api::LogonUser($user, $domain, $password, 9, 0, [ref]$token) | Out-Null
+	$identity = New-Object Security.Principal.WindowsIdentity $token
+	$context = $Identity.Impersonate()
 
+	
 	while($True) {
 		try {
-			New-PSDrive -name X -PSProvider FileSystem -root \\FAKEDOMAIN\C$ -Credential $creds -ErrorAction Ignore -ErrorVariable $err
-			Invoke-WebRequest fakedomain.local -Credential $creds -ErrorAction Ignore -ErrorVariable $err
-
+			Write-Verbose "Mapping Drive"
+			New-PSDrive -name X -PSProvider FileSystem -root \\$($domain)\C$ -Credential $creds -ErrorAction Ignore -ErrorVariable $err
+			
+			Write-Verbose "Requesting http://$($domain).local with Basic Auth"
 			$headers = @{
-				Authorization = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("dave:ILoveBen!"))
+				Authorization = "Basic " + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($plain))
 			}
 
-			Invoke-WebRequest fakedomain.local -Credential $creds -Headers $headers -ErrorAction Ignore -ErrorVariable $err	
+			Invoke-WebRequest "http://$($domain).local" -Headers $headers
 		}
 		catch {
 			
